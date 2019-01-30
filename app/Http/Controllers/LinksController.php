d09a91c30fc19a20ccf656f920783ede447a2eb6
almar_mg <Almar.gemmel@live.nl>
2019-01-30 12:20:13 +0100

    update toets2

---
+++ app/Http/Controllers/LinksController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Link;
+use Illuminate\Http\Request;
+
+class LinksController extends Controller
+{
+  public function index(){
+
+      $links = Link::all();
+
+      return view('links',compact("links"));
+}
+
+  public function showLinksForm(){
+    return view('LinksForm');
+  }
+
+  public function handleLinksForm(Request $request){
+
+  }
+
+}
+
+
+
